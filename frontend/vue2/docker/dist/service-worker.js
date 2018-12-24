0154d772651b05e4b24966adb97b2796c3e1ac92
Satyendra Kumar <kumarsatya1990@gmail.com>
2018-12-24 16:27:18 +0530

    ace-min added in frontend

--- frontend/vue2/docker/dist/service-worker.js
+++ frontend/vue2/docker/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e272346bd1a2ebb424fad7326757c34f.js"
+  "/precache-manifest.7995fb974bcfb9718f21f0aef5801fa4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "realworld-vue"});
