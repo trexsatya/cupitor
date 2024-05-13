31cee97decd14555b5300bbeebd6ea1f61a2243d
Satyendra Kumar <kumarsatya1990@gmail.com>
2024-05-13 09:51:55 +0200

    Update UI

--- frontend/vue3/cupitor-frontend/public/data-structures.js
+++ frontend/vue3/cupitor-frontend/public/data-structures.js
@@ -15,6 +15,21 @@ function range(start, count, filter, fn) {
   if(fn) return ar.filter(filter).map(x => fn(x))
   else return ar;
 }
+
+function uuid() {
+  const S4 = function () {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+function computeIfAbsent(map, key, fn) {
+  if(!map[key]) {
+    map[key] = fn()
+  }
+  return map[key]
+}
+
 function CircularCursor(arr) {
   this.items = arr
   this.currentIndex = -1
